394a097939e0f2d3ef084c42db9a672c58f01210
Катя <kaaate.ivashchenko@gmail.com>
2022-09-13 21:10:41 +0300

    BTC Exchange for Prog Academy - done!

---
+++ bitcoinexchange.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+class BitcoinExchange:
+
+    __BTC_price = 0
+    __USD = 0
+
+    def __init__(self, BTC, USD):
+        self.set_values(BTC, USD)
+        self.get_BTC_price()
+
+    def set_values(self, BTC, USD):
+        self.__BTC_price = BTC
+        self.__USD = USD
+
+    def get_BTC_price(self):
+        return self.__BTC_price
+
+    def get_USD(self):
+        return self.__USD
+
+    def __str__(self):
+        return f"You can buy {self.bitcoin_exchange_value()} BTC"
+
+
+    def bitcoin_exchange_value(self):
+        result = int(self.get_USD()) / int(self.get_BTC_price())
+        return round(result, 7)
+
+if __name__ == '__main__':
+
+    BTC_price = float(input("What is Bitcoin price today?\n"))
+    USD = float(input("How much $ do you have?\n"))
+    print(BitcoinExchange(BTC=BTC_price, USD=USD))
+
+
